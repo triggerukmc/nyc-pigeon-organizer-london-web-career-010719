5b7000527669fd3a6c0c2e5830cb578d6b64d1b1
Matthew Collins <matthew.collins1991@yahoo.com>
2019-01-06 19:39:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,24 @@
 require "pry"
 
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
 def nyc_pigeon_organizer(input)
   pigeon_data = {}
    	  input.each do |attribute, data|
@@ -21,4 +40,4 @@ def nyc_pigeon_organizer(input)
   pigeon_data
 end
 
-nyc_pigeon_organizer(input)
+nyc_pigeon_organizer(pigeon_data)
