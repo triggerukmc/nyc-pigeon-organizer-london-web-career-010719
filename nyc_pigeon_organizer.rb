e875bbf73788a51479d80b0fd6b14183463a8c9f
Matthew Collins <matthew.collins1991@yahoo.com>
2019-01-06 19:31:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(input)
+  pigeon_data = {}
+   	  input.each do |attribute, data|
+    data.each do |quality, names|
+      names.each do |name|
+        if pigeon_data[name]
+          if pigeon_data[name][attribute]
+            pigeon_data[name][attribute] << quality.to_s
+          else
+            pigeon_data[name][attribute] = [quality.to_s]
+          end
+        else
+          pigeon_data[name] = {attribute => [quality.to_s]}
+        end
+      end
+    end
+  end
+  pigeon_data
 end
