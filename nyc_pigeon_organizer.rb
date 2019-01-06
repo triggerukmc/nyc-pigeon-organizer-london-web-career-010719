124fde629369879a550935bc3b1fb182c464d978
Matthew Collins <matthew.collins1991@yahoo.com>
2019-01-06 19:41:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,38 +1,18 @@
-require "pry"
 
-pigeon_data = {
-  :color => {
-    :purple => ["Theo", "Peter Jr.", "Lucky"],
-    :grey => ["Theo", "Peter Jr.", "Ms. K"],
-    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-    :brown => ["Queenie", "Alex"]
-  },
-  :gender => {
-    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-    :female => ["Queenie", "Ms. K"]
-  },
-  :lives => {
-    "Subway" => ["Theo", "Queenie"],
-    "Central Park" => ["Alex", "Ms. K", "Lucky"],
-    "Library" => ["Peter Jr."],
-    "City Hall" => ["Andrew"]
-  }
-}
 
 def nyc_pigeon_organizer(input)
   pigeon_data = {}
    	  input.each do |attribute, data|
-    data.each do |quality, names|
-      binding.pry
+    data.each do |color, names|
       names.each do |name|
         if pigeon_data[name]
           if pigeon_data[name][attribute]
-            pigeon_data[name][attribute] << quality.to_s
+            pigeon_data[name][attribute] << color.to_s
           else
-            pigeon_data[name][attribute] = [quality.to_s]
+            pigeon_data[name][attribute] = [color.to_s]
           end
         else
-          pigeon_data[name] = {attribute => [quality.to_s]}
+          pigeon_data[name] = {attribute => [color.to_s]}
         end
       end
     end
